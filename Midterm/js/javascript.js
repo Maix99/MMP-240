1be1818a3c10a7f052f691734142219bc08f4444
Mai <xdarkchiix@gmail.com>
2015-04-13 15:51:54 -0400

    midterm assignment
    adding my midterm assignment into github pages

---
+++ Midterm/js/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#tribeca-loft img').mouseenter(function(){
+                 $('#tribeca-loft h5').css("opacity", "1");
+    });
+    $('#tribeca-loft img').mouseleave(function(){
+                 $('#tribeca-loft h5').css("opacity", "0");
+    });
+}
